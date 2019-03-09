fc5b127d8061dcf542648ec9ae8cd44c622bda60
Jugav <jugav407@gmail.com>
2019-03-09 17:40:09 +0530

    write csv using csv writer

--- src/main/java/com/aws/ETL/ExcelToCSV.java
+++ src/main/java/com/aws/ETL/ExcelToCSV.java
@@ -1,9 +1,8 @@
 package com.aws.ETL;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,51 +12,49 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-public class ExcelToCSV {
-	public static String[] convertToCSV(String xlsxFile,String filename, String csvoutput, String Outputpath) throws InvalidFormatException, IOException {
+import com.opencsv.CSVWriter;
 
-		//Workbook wb = new XSSFWorkbook(new File(xlsxFile));
+public class ExcelToCSV {
+	private static final char COMMA_DELIMITER = ';';
+	public static void convertToCSV(String xlsxFile,String filename, String csvoutput, String Outputpath) throws InvalidFormatException, IOException {
+
 		Workbook wb = WorkbookFactory.create(new File(xlsxFile));
+
 		DataFormatter formatter = new DataFormatter();
-		//PrintStream out ;
-                PrintStream out2 ;
-		String file[] = new String[wb.getNumberOfSheets()];
-		int i = 0;
-                
-                File file3 = new File(csvoutput+"\\csv");
-                if (!file3.exists()) {
-                    if (file3.mkdir()) {
-                        System.out.println("csv Directory is created!");
-                    } else {
-                        System.out.println("csv Failed to create directory!");
-                    }
-                }
-                
-                File file2 = new File(Outputpath+"\\csv");
-                if (!file2.exists()) {
-                    if (file2.mkdir()) {
-                        System.out.println("csv Directory is created!");
-                    } else {
-                        System.out.println("csv Failed to create directory!");
-                    }
-                }
-                                    
-		for (Sheet sheet : wb) {
-			out2 = new PrintStream(new FileOutputStream(Outputpath +"\\csv\\"+filename+sheet.getSheetName()+".csv"),
-	                true, "UTF-8");
-			file[i] = Outputpath + "\\csv\\"+filename+sheet.getSheetName()+".csv";
-			for (Row row : sheet) {
-				boolean firstCell = true;
-				for (Cell cell : row) {
-					if ( ! firstCell )out2.print(';'); 
-                          String text = formatter.formatCellValue(cell);
-					      out2.print(text);
-					      firstCell = false;
-				}
-                                out2.println();
-			}
-			i++;
-		}
-		return file;
+          
+          File file2 = new File(Outputpath+"\\csv");
+          if (!file2.exists()) {
+              if (file2.mkdir()) {
+                  System.out.println("csv Directory is created!");
+              } else {
+                  System.out.println("csv Failed to create directory!");
+              }
+          }
+          FileWriter fileWriter ;
+          CSVWriter csvwriter ;
+		for(Sheet sheet : wb) {
+			File f = new File(Outputpath + "\\csv\\"+filename+sheet.getSheetName()+".csv");
+			 fileWriter = new FileWriter(f);
+			 csvwriter = new CSVWriter(fileWriter, COMMA_DELIMITER, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+				
+	        for (Row row : sheet) {
+	        	String nextLine[] = new String[row.getPhysicalNumberOfCells()];
+	        	int k=0;
+	            for (Cell cell : row) {
+	            	String cellValue = formatter.formatCellValue(cell);
+	            	nextLine[k] = cellValue;
+	            	k++;
+	            }
+	            csvwriter.writeNext(nextLine);
+	        }
+	        try {
+                csvwriter.close();
+            } catch (IOException e) {
+                System.out.println("Error while flushing/closing fileWriter !!!");
+                e.printStackTrace();
+            }
+            
+        }
 	}
+	
 }
--- src/main/java/com/aws/ETL/XMLParser.java
+++ src/main/java/com/aws/ETL/XMLParser.java
@@ -32,10 +32,10 @@ import org.w3c.dom.NodeList;
 public class XMLParser 
 {
     //Delimiter used in CSV file
-    private static final String DELIMITER = ";";
-    private static final String ORDER_FILE_HEADER = "Transaction Number; Active Price; Active Price Description; Department Code; Sub Department Code; Total Amount Paid; Total Units; Product Name, UPC Code";
-    private static final String TRS_FILEHEADER = "Transaction Number for R; Transaction Number for T; InvoiceNumber; CustomerID; CustomerName; TransactionMode; TransactionStartDate; TransactionEndDate; TerminalCode; TerminalStoreCode; OperatorNumber; OperatorName";
-    private static final String T_FILEHEADER = "Transaction Number; Totalizer Code; Ticket Concept; Total Amount; Total Unit; Total Weight";
+    private static final String DELIMITER = ",";
+    private static final String ORDER_FILE_HEADER = "Transaction Number, Active Price, Active Price Description, Department Code, Sub Department Code, Total Amount Paid, Total Units, Product Name, UPC Code";
+    private static final String TRS_FILEHEADER = "Transaction Number for R, Transaction Number for T, InvoiceNumber, CustomerID, CustomerName, TransactionMode, TransactionStartDate, TransactionEndDate, TerminalCode, TerminalStoreCode, OperatorNumber, OperatorName";
+    private static final String T_FILEHEADER = "Transaction Number, Totalizer Code, Ticket Concept, Total Amount, Total Unit, Total Weight";
     static List<TRS> trsList;
     
     public static String parseXML(String INPUT_PATH, String OUTPUT_PATH) {
@@ -254,7 +254,7 @@ public class XMLParser
                                     workbook.write(fileOut);
                                     fileOut.close();
                                     
-                                    String files[] = ExcelToCSV.convertToCSV(csvfilepath,filename2.replaceAll(" ", "_").toLowerCase()+"_", csvoutput, OUTPUT_PATH);
+                                    ExcelToCSV.convertToCSV(csvfilepath,filename2.replaceAll(" ", "_").toLowerCase()+"_", csvoutput, OUTPUT_PATH);
                     	  }
                     	  catch(Exception e) {
                   			e.printStackTrace();
